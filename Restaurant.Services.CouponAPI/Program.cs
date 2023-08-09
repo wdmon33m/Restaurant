7035de1e8141a4a3b14bf1167940b11098434025
wdmon33m <wdmon33m@gmail.com>
2023-08-08 21:30:59 -0500

    add migration to add coupon table

--- Restaurant.Services.CouponAPI/Program.cs
+++ Restaurant.Services.CouponAPI/Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefualtConnection"));
-    options.UseSqlServer("");
 });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
