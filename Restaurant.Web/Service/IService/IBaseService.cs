a7233461710e2e905881f15663fc383ddf7e741c
wdmon33m <wdmon33m@gmail.com>
2023-08-09 02:38:27 -0500

    Add IBaseService and BaseService

---
+++ Restaurant.Web/Service/IService/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿using Restaurant.Web.Models;
+
+namespace Restaurant.Web.Service.IService
+{
+    public interface IBaseService
+    {
+        Task<ResponseDto?> SendAsync(RequestDto requestDto);
+    }
+}
