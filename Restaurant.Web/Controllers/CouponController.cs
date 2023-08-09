882d7c9ac6a23f29fad66ee81841b01a630dec80
wdmon33m <wdmon33m@gmail.com>
2023-08-09 04:01:28 -0500

    Add Coupon Controller to Web Project

---
+++ Restaurant.Web/Controllers/CouponController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Restaurant.Web.Models;
+using Restaurant.Web.Service.IService;
+
+namespace Restaurant.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto>? list = new();
+            ResponseDto? response = await _couponService.GetAllCouponsAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+
+            return View(list);
+        }
+    }
+}
