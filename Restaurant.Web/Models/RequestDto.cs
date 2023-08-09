318d9bf04211c01072ec1005cda18809e07ab424
wdmon33m <wdmon33m@gmail.com>
2023-08-09 01:50:03 -0500

    Add Request Dto class

---
+++ Restaurant.Web/Models/RequestDto.cs
@@ -0,0 +1,13 @@
+﻿using Restaurant.Web.Utility;
+using static Restaurant.Web.Utility.SD;
+
+namespace Restaurant.Web.Models
+{
+    public class RequestDto
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
