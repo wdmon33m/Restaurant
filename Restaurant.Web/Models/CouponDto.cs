4612adf8b71fd0df737f9c437f7337b47c19b4a8
wdmon33m <wdmon33m@gmail.com>
2023-08-09 02:39:44 -0500

    Add Copon Dto to Web Project

---
+++ Restaurant.Web/Models/CouponDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurant.Web.Models
+{
+    public class CouponDto
+    {
+        public int CouponID { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public double MinAmount { get; set; }
+
+    }
+}
