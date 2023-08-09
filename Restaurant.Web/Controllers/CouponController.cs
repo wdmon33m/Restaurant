09d28c4441b94c0b2da14d1d2cd06f2694533f06
wdmon33m <wdmon33m@gmail.com>
2023-08-09 13:24:03 -0500

    Add Create Controller and View for Coupon in web

--- Restaurant.Web/Controllers/CouponController.cs
+++ Restaurant.Web/Controllers/CouponController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using Restaurant.Web.Models;
 using Restaurant.Web.Service.IService;
+using System.Collections.Generic;
 
 namespace Restaurant.Web.Controllers
 {
@@ -25,6 +26,47 @@ namespace Restaurant.Web.Controllers
             }
 
             return View(list);
+        }
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto couponDto)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponService.CreateCouponAsync(couponDto);
+
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+            return View(couponDto);
+        }
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+            ResponseDto? response = await _couponService.GetCouponByIdAsync(couponId);
+
+            if (response != null && response.IsSuccess)
+            {
+                CouponDto? couponDto = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result));
+                return View(couponDto);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CouponDelete(CouponDto couponDto)
+        {
+            ResponseDto? response = await _couponService.DeleteCouponAsync(couponDto.CouponID);
+
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(CouponIndex));
+            }
+
+            return View(response);
         }
     }
 }
