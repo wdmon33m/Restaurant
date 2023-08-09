e498c85967b4e831323e00bb0a2e265064b4776a
wdmon33m <wdmon33m@gmail.com>
2023-08-09 01:50:38 -0500

    Add Static Details (SD) class

---
+++ Restaurant.Web/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace Restaurant.Web.Utility
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
