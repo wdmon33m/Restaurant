fc6a2ef33ed5f3df88e31372a806cb8b7eba86ca
wdmon33m <wdmon33m@gmail.com>
2023-08-09 21:53:10 -0500

    add new dtos to auth project

---
+++ Restaurant.Services.AuthAPI/Models/Dto/RegistrationRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Restaurant.Services.AuthAPI.Models.Dto
+{
+    public class RegistrationRequestDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
