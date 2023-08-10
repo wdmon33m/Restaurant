c58ae1a4e5dbac84a671b9bfebb3c2d6d659f7e2
wdmon33m <wdmon33m@gmail.com>
2023-08-09 21:51:55 -0500

    add auth api contoller to auth project

---
+++ Restaurant.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant.Services.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+    }
+    
+
+}
