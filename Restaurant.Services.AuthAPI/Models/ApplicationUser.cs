ed67e7426d33e4fe1b7c26c49341924d44571e60
wdmon33m <wdmon33m@gmail.com>
2023-08-09 21:53:34 -0500

    add model classes

---
+++ Restaurant.Services.AuthAPI/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Restaurant.Services.AuthAPI.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName{ get; set; }
+        public string LastName { get; set; }
+    }
+}
