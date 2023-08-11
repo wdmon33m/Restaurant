74aa5dd2d7cf0244fba31968d64aada0df62afac
wdmon33m <wdmon33m@gmail.com>
2023-08-10 19:40:11 -0500

    Remove ResponseDto From AuthAPI

--- Restaurant.Services.AuthAPI/Models/Dto/ResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Restaurant.Services.AuthAPI.Models.Dto
-{
-    public class ResponseDto
-    {
-        public bool IsSuccess { get; set; } = true;
-        public List<string> ErrorMessages { get; set; }
-        public object Result { get; set; }
-    }
-}
